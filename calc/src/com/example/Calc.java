e0f11bd60222522b54595e40dfdaf216760d50d6
ErReich <nikolya.severus@mail.ru>
2020-08-26 15:31:33 +0300

    HW1 fixing mistakes

--- calc/src/com/example/Calc.java
+++ calc/src/com/example/Calc.java
@@ -7,16 +7,18 @@ public class Calc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(
-				"Введите операцию (без кавычек): \n'+' (сложение), \n‘-’ (разность), \n‘*’ (умножение), \n‘/’ (деление), \n‘div’ (целочисленное деление), \n‘mod’ (остаток от деления), \n‘^’ (возведение в степень)");
+				"Введите операцию (без кавычек): \n'+' (сложение), \n‘-’ (разность), \n‘*’ (умножение), \n‘/’ (деление),"
+				+ " \n‘div’ (целочисленное деление), \n‘mod’ (остаток от деления), \n‘^’ (возведение в степень)");
 
 		Scanner in = new Scanner(System.in);
 
 		var operation = in.nextLine();
 
-		for (; !("+".equals(operation)) && !("-".equals(operation)) && !("*".equals(operation))
+		while (!("+".equals(operation)) && !("-".equals(operation)) && !("*".equals(operation))
 				&& !("/".equals(operation)) && !("div".equals(operation)) && !("mod".equals(operation))
-				&& !("^".equals(operation)); operation = in.nextLine()) {
+				&& !("^".equals(operation))) {
 			System.out.println("Введена некорректная операция, попробуйте еще раз");
+			operation = in.nextLine();
 		}
 
 		System.out.println("Введите первое число");
@@ -28,26 +30,25 @@ public class Calc {
 		switch (operation) {
 
 		case "+": {
-			System.out.println(number1 + "+" + number2 + "=" + (number1 + number2));
+			System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
 			break;
 		}
 
 		case "-": {
-			System.out.println(number1 + "-" + number2 + "=" + (number1 - number2));
+			System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
 			break;
 		}
 
 		case "*": {
-			System.out.println(number1 + "*" + number2 + "=" + (number1 * number2));
+			System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
 			break;
 		}
 
 		case "/": {
-			var number3 = (float) number1;
 			if (number2 == 0) {
 				System.out.print("Недопустимая операция");
 			} else {
-				System.out.println(number1 + "/" + number2 + "=" + (number3 / number2));
+				System.out.println(number1 + " / " + number2 + " = " + ((float) number1 / number2));
 			}
 			break;
 		}
@@ -56,7 +57,7 @@ public class Calc {
 			if (number2 == 0) {
 				System.out.print("Недопустимая операция");
 			} else {
-				System.out.println(number1 + "div" + number2 + "=" + (number1 / number2));
+				System.out.println(number1 + " div " + number2 + " = " + (number1 / number2));
 			}
 			break;
 		}
@@ -65,25 +66,30 @@ public class Calc {
 			if (number2 == 0) {
 				System.out.print("Недопустимая операция");
 			} else {
-				System.out.println(number1 + "mod" + number2 + "=" + (number1 % number2));
+				System.out.println(number1 + " mod " + number2 + " = " + (number1 % number2));
 			}
 			break;
 		}
 
 		case "^": {
-			var result = (float) 1;
+			var result = 1f;
 			for (var i = 1; i <= Math.abs(number2); i++) {
 				result = result * number1;
 			}
 			if (number2 < 0) {
-				System.out.println(number1 + "^" + number2 + "=" + (1 / result));
+				System.out.println(number1 + " ^ " + number2 + " = " + (1 / result));
 			} else if (number1 == 0 && number2 == 0) {
-				System.out.println(number1 + "^" + number2 + "=" + Double.POSITIVE_INFINITY);
+				System.out.println(number1 + " ^ " + number2 + " = " + Double.POSITIVE_INFINITY);
 			} else {
-				System.out.println(number1 + "^" + number2 + "=" + result);
+				System.out.println(number1 + " ^ " + number2 + " = " + result);
 			}
 			break;
 		}
+		
+		default: {
+			System.out.println("Unreasonable default case");
+		}
+		
 		}
 	}
 
