51746e6e40533832be5c6ca8e56fa8b40308bdcd
ErReich <nikolya.severus@mail.ru>
2020-08-28 14:19:17 +0300

    Убрал float для высоты рамки

--- FrameTry/src/com/example/FrameTry.java
+++ FrameTry/src/com/example/FrameTry.java
@@ -8,10 +8,11 @@ public class FrameTry {
 		// TODO Auto-generated method stub
 		var in = new Scanner(System.in);
 		
-		var heigthOfFrame = in.nextFloat();
+		var heigthOfFrame = in.nextInt();
 		var lengthOfFrame = in.nextInt();
 		var stringOfFrame = in.next();
-		var ends = (lengthOfFrame - stringOfFrame.length()) / 2;    // ---
+		var ends = (lengthOfFrame - stringOfFrame.length()) / 2;
+		boolean parity = heigthOfFrame % 2 == 0;
 		
 		if (heigthOfFrame < 3 || (lengthOfFrame - 2) < stringOfFrame.length()) {
 			System.out.println("Ошибка");
@@ -23,7 +24,10 @@ public class FrameTry {
 					} else {
 						System.out.print(" ");
 					}
-					if (i == Math.ceil(heigthOfFrame/2) && j == ends) {
+					if (parity == true && i == heigthOfFrame / 2 && j == ends) {
+						System.out.print(stringOfFrame);
+						j += stringOfFrame.length();
+					} else if (parity == false && i == heigthOfFrame / 2 + 1 && j == ends) {
 						System.out.print(stringOfFrame);
 						j += stringOfFrame.length();
 					}
